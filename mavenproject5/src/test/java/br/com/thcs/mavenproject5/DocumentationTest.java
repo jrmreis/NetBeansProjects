c2b80b5a2b1f3d521bfa7a21d2fdd85b857c9871
Joel R M Reis <69910076+jrmreis@users.noreply.github.com>
2020-10-02 17:05:18 -0300

    sextou

--- mavenproject5/src/test/java/br/com/thcs/mavenproject5/DocumentationTest.java
+++
@@ -1,7 +0,0 @@
-package br.com.thcs.mavenproject5;
-
-import com.github.jcustenborder.kafka.connect.utils.BaseDocumentationTest;
-
-public class DocumentationTest extends BaseDocumentationTest {
-
-}
